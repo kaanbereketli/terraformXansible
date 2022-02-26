ec757bfec318d8afdb7b230a14e4acf34da18543
kaanbereketli <kaanbereketli1995@gmail.com>
2022-02-26 17:02:58 +0000

    prometheus installation complete

--- compute.tf
+++ compute.tf
@@ -47,21 +47,6 @@ resource "aws_instance" "green_main" {
   }
 }
 
-# #DON'T USE THIS. It doesn't wait for Grafana to be bootstrapped.
-# resource "null_resource" "grafana_update" {
-#   count = var.main_instance_count
-#   provisioner "remote-exec" {
-#     inline = ["sudo apt upgrade -y grafana && touch upgrade.log && echo 'I upgraded Grafana' >> upgrade.log"]
-
-#     connection {
-#       type        = "ssh"
-#       user        = "ubuntu"
-#       private_key = file("/home/ubuntu/.ssh/id_rsa")
-#       host        = aws_instance.green_main[count.index].public_ip
-#     }
-#   }
-# }
-
 resource "null_resource" "grafana_install" {
   #We don't specify any counts here. So it will wait all the green_main intances are created.
   depends_on = [aws_instance.green_main]
@@ -70,3 +55,6 @@ resource "null_resource" "grafana_install" {
   }
 }
 
+output "grafana_access" {
+  value = {for i in aws_instance.green_main[*] : i.tags.Name => "${i.public_ip}:3000"}
+}
